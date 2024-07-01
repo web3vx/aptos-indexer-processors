ddce08eff5f15cf8ea27248c33bea5d01ada772d
tx-tomcat <dungnd8594@gmail.com>
2024-07-01 17:40:53 +0700

    remove contrainst

--- rust/processor/migrations/2024-07-01-102955_remove-multisig_voting_contraints/up.sql
+++ rust/processor/migrations/2024-07-01-102955_remove-multisig_voting_contraints/up.sql
@@ -1,4 +1,5 @@
 -- Your SQL goes here
-ALTER TABLE multisig_transactions DROP CONSTRAINT multisig_voting_transactions_voter_address_fkey;
-alter table public.multisig_voting_transactions
+ALTER TABLE multisig_voting_transactions DROP CONSTRAINT multisig_voting_transactions_voter_address_fkey;
+alter table multisig_voting_transactions
     drop constraint multisig_voting_transactions_wallet_address_transaction_se_fkey;
+
