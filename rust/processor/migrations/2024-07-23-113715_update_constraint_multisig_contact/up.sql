3223f49d777f9c84575963da4bec40534b3925ab
tx-tomcat <dungnd8594@gmail.com>
2024-07-24 14:30:33 +0700

    add parsing complex arg

--- rust/processor/migrations/2024-07-23-113715_update_constraint_multisig_contact/up.sql
+++ rust/processor/migrations/2024-07-23-113715_update_constraint_multisig_contact/up.sql
@@ -1,4 +1,13 @@
 -- Your SQL goes here
-ALTER TABLE multisig_contacts RENAME COLUMN owner_address TO wallet_address;
+drop table if exists multisig_contacts;
+
+CREATE TABLE multisig_contacts (
+   id SERIAL PRIMARY KEY NOT NULL,
+   wallet_address VARCHAR(255) NOT NULL,
+   contact_address VARCHAR(255) NOT NULL,
+   contact_name VARCHAR(255) NOT NULL,
+   created_at TIMESTAMP DEFAULT NOW() NOT NULL
+);
+
 ALTER TABLE multisig_contacts ADD CONSTRAINT multisig_contacts_uniq UNIQUE (wallet_address, contact_address);
 ALTER TABLE multisig_contacts ADD CONSTRAINT fk_multisig_contacts FOREIGN KEY (wallet_address) REFERENCES multisig_wallets (wallet_address);
